8835e673d4e0ff3b07be56cbe6e53cca2c349114
nicoP08 <pnsdaaf@gmail.com>
2018-07-10 14:07:19 +0200

    aa

--- index.php
+++ index.php
@@ -18,16 +18,60 @@
 
   <!-- carousel-->
 
-  <div class="wrapper">
-    <div class="carousel-container">
-      <div class="arrow left-arrow"><i class="fa fa-arrow-circle-left fa-3x"></i></div>
-      <ul class="carousel">
-        <li><img src="img/affiche1.jpg"></li>
-        <li><img src="img/affiche2.jpg"></li>
-        <li><img src="img/affiche3.jpeg"></li>
-      </ul>
-      <div class="arrow right-arrow"><i class="fa fa-arrow-circle-right fa-3x"></i></div>
+  <div class="contain">
+
+  
+
+    <div class="row">
+      <div class="row__inner">
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="img/affiche4.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+
+            </div>
+          </div>
+        </div>Thor
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="img/affiche1.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              CAPTAIN AMERICA
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="img/affiche2.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              IRON MAN
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="img/affiche3.jpeg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              IRON MAN 2
+            </div>
+          </div>
+        </div>
+
+      </div>
     </div>
+
   </div>
   </body>
  </html>
