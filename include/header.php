9c08f1418c00c36230d60e1e994223e3cf149d7b
nicoP08 <pnsdaaf@gmail.com>
2018-07-10 12:16:31 +0200

    a

---
+++ include/header.php
@@ -0,0 +1,11 @@
+
+
+  <body>
+    <main>
+      <div id="header">
+      <a href="index.php">  <img src="img/logo.jpg" class="logo" alt="métropolis" width="100px" height="100px;"></a>
+        <h1>METRO VOD</h1>
+        <button>Connexion</button>
+        <button>Inscription</button>
+      </div>
+  </body>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf8">
+    <link rel="stylesheet" media="screen" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" media="screen" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" media="screen" type="text/css" href="css/header.css">
+<script src="js/scripts.js"></script>
+  </head>
+
+  <body>
+
+
+  <main>
+    <!-- header-->
+  <?php include("include/header.php") ?>
+  <!-- end header-->
+
+  <!-- carousel-->
+
+  <div class="wrapper">
+    <div class="carousel-container">
+      <div class="arrow left-arrow"><i class="fa fa-arrow-circle-left fa-3x"></i></div>
+      <ul class="carousel">
+        <li><img src="img/affiche1.jpg"></li>
+        <li><img src="img/affiche2.jpg"></li>
+        <li><img src="img/affiche3.jpeg"></li>
+      </ul>
+      <div class="arrow right-arrow"><i class="fa fa-arrow-circle-right fa-3x"></i></div>
+    </div>
+  </div>
+  </body>
+ </html>
